6de77faae7dd1f9774c1d7241b2e580b0273e2a4
Walter, Mona <mona.walter@tecalliance.net>
2022-09-21 09:30:12 +0200

    Sum of strings -> to int

--- Uebung/Uebung/Program.cs
+++ Uebung/Uebung/Program.cs
@@ -10,20 +10,11 @@ namespace Uebung
     {
         static void Main(string[] args)
         {
-            //List<string> azubiNamen = new List<string>() { "Nana", "Almond", "Norbert"};
-
-            //List<string> NamesWithN = GiveMeTheNames(azubiNamen);
-
-            //foreach(string name in NamesWithN)
-            //{
-            //    Console.WriteLine(name);
-            //}
-
             Console.WriteLine("Gebe mir eine Zahl");
-            int zahl1 = Convert.ToInt32(Console.ReadLine());
+            string zahl1 = Console.ReadLine();
 
             Console.WriteLine("Gebe mir eine weitere Zahl");
-            int zahl2 = Convert.ToInt32(Console.ReadLine());
+            string zahl2 = Console.ReadLine();
 
             //wenn das Ergebnis nochmal gebraucht wird,dann Addition(zahl1, zahl2) in einer Variable speichern
             Console.WriteLine($"Die Summe von {zahl1} und {zahl2} ist: {Addition(zahl1, zahl2)}");
@@ -31,24 +22,10 @@ namespace Uebung
             Console.ReadLine();
         }
         
-        static int Addition(int z1, int z2)
+        static int Addition(string z1, string z2)
         {
-            int summe = z1 + z2;
+            int summe = Convert.ToInt32(z1) + Convert.ToInt32(z2);
             return summe;
         }
-
-        //static List<string> GiveMeTheNames(List<string> AllTrainees)
-        //{
-        //    List<string> allTraineesWithN = new List<string>();
-
-        //    foreach(string trainee in AllTrainees)
-        //    {
-        //        if (trainee.StartsWith("N"))
-        //        {
-        //            allTraineesWithN.Add(trainee);
-        //        }
-        //    }
-        //    return allTraineesWithN;
-        //}
     }
 }
